7c858f9a08c05643da28fbfebd3b224943f22759
kafapilot <angelikaozgurluk404@gmail.com>
2024-09-12 14:16:18 +0300

    Add files via upload

---
+++ pw_strenght.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	"use strict";
+	var password1 		= $('#password1'); //id of first password field
+	var password2		= $('#password2'); //id of second password field
+	var passwordsInfo 	= $('#pass-info'); //id of indicator element
+	
+	passwordStrengthCheck(password1,password2,passwordsInfo); //call password check function
+	
+});
+
+function passwordStrengthCheck(password1, password2, passwordsInfo)
+{
+	//Must contain 6 characters or more
+	var WeakPass = /(?=.{6,}).*/; 
+	//Must contain lower case letters and at least one digit.
+	var MediumPass = /^(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/; 
+	//Must contain at least one upper case letter, one lower case letter and one digit.
+	var StrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/; 
+	//Must contain at least one upper case letter, one lower case letter and one digit.
+	var VryStrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[^\w\*])\S{6,}$/; 
+	
+	$(password1).on('keyup', function(e) {
+		if(VryStrongPass.test(password1.val()))
+		{
+			passwordsInfo.removeClass().addClass('weakpass').html();
+		}	
+		
+		
+	});
+	
+	$(password2).on('keyup', function(e) {
+		if(password1.val() !== password2.val())
+		{
+			passwordsInfo.removeClass().addClass('weakpass').html("Passwords do not match!");	
+		}else{
+			passwordsInfo.removeClass().addClass('goodpass').html("Passwords match!");	
+		}
+			
+	});
+}
